8ad6f2d30b8af6f5a7c546103ce214406db36815
C1834704 <McSweeneyC2@cardiff.ac.uk>
2018-10-21 17:42:35 +0100

    Completed animation functionality and added bear fight animation.

--- animation.py
+++ animation.py
@@ -5,69 +5,58 @@ import time
 import base64
 import sys
 
+import anim_bear
+
 # Border characters - TL, T, TR, R, BR, B, BL, L
 anim_border = "╔═╗║╝═╚║"
-
-def decode_row(row, width):
-    decoded = base64.decodebytes(row.encode("ascii"))
-    print(decoded.decode("ascii"))
     
-def print_animation_frame(frame, width, invert):
-    """This function takes a frame (a list of strings containing base64 encoded
-    brightness values) and prints them in a rectangular frame. Optionally invert
-    for light-themed terminals such as IDLE."""
+def print_animation_frame(frame):
+    """This function takes a frame (a list of strings) and prints them in a
+    rectangular frame."""
 
     global anim_border
 
+    # Compute the frame width
+    anim_width = 0
+    for row in frame:
+        if len(row) > anim_width:
+            anim_width = len(row)
+
     # Clear the screen
-    frame_text = "\n" * 500
+    frame_text = "\n" * 50
     # Add the top border
-    frame_text += anim_border[0] + (anim_border[1] * width) + anim_border[2] + "\n"
+    frame_text += anim_border[0] + (anim_border[1] * anim_width) + anim_border[2] + "\n"
     
     # Add each row with side borders
     for row in frame:
         frame_text += anim_border[7]
         frame_text += row
-        if len(row) < width:
-            frame_text += " " * (width - len(row))
+        if len(row) < anim_width:
+            frame_text += " " * (anim_width - len(row))
         frame_text += anim_border[3] + "\n"
 
     # Add the bottom border
-    frame_text += anim_border[6] + (anim_border[5] * width) + anim_border[4]
+    frame_text += anim_border[6] + (anim_border[5] * anim_width) + anim_border[4] + "\n"
 
-    # Print everything at once
-    print(frame_text)
+    # Print everything at once.
+    # Using stdout doesn't add a newline afterwards, preventing jitter.
+    sys.stdout.write(frame_text)
 
-def print_animation(frames, width, rate):
-    """This function takes a list of frames (see above) and prints
-    them in a rectangular frame at a given frame rate (frames per second)"""
+def print_animation(anim):
+    """This function takes an animation object and prints its frames in a
+    rectangular frame at a given frame rate (frames per second)"""
+
+    # Predelay of half a second
+    time.sleep(0.5)
+    
     # Compute delay time
+    rate = anim["rate"]
     if rate < 0.2:
        rate = 0.2
     anim_delay = 1.0/rate
 
     # Print each frame at the specified rate (approximate)
-    for frame in frames:
-        print_animation_frame(frame, width)
-        time.sleep(anim_delay)
-
-testanim = [
-	[
-		"Hello Hello",
-		"lo Hello Hello Hello Hel",
-		"Hello Hello Hello Hello ",
-		"lo Hello Hello Hello Hel",
-		"Hello Hello Hello Hello ",
-		"lo Hello Hello Hello Hel"
-	],[
-		"World World World World ",
-		"ld World World World Wor",
-		"World World World World ",
-		"ld World World World Wor",
-		"World World World World ",
-		"ld World World World Wor"
-	]
-]
-
-print_animation(testanim, 24, 2.5)
-
+    for loop_number in range(0,anim["repeat"]):
+        for frame_number in anim["order"]:
+            print_animation_frame(anim["frames"][frame_number])
+            time.sleep(anim_delay)
